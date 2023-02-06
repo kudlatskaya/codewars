c480a39490e4313ce8c20f55ed933fb2a2bbf2eb
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-02-06 15:29:24 +0300

    C53

--- index.tsx
+++ index.tsx
@@ -1,4 +1,9 @@
 export function simpleMultiplication(num: number): number{
 
-    return (num % 2) ? num * 8 : num * 9;
+    return (num % 2) ? num * 9 : num * 8;
+}
+
+export function findNeedle(haystack: any[]):string {
+    let index = haystack.findIndex((item) => item == 'needle');
+    return  `found the needle at position ${index}`;
 }
