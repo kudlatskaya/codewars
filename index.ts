b95eb3c1c164daf25505c8afc92853a5ce9e92f6
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-02-10 20:32:40 +0300

    c54

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+
 export function simpleMultiplication(num: number): number{
 
     return (num % 2) ? num * 9 : num * 8;
@@ -6,4 +7,15 @@ export function simpleMultiplication(num: number): number{
 export function findNeedle(haystack: any[]):string {
     let index = haystack.findIndex((item) => item == 'needle');
     return  `found the needle at position ${index}`;
+}
+
+export function sumCubes(n: number): number {
+    // your code here
+    let sum: number = 0;
+
+    for(let i = 1; i <= n; i++) {
+        sum += i**3;
+    }
+
+    return sum
 }
